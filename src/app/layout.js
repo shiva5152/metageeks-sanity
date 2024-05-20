477a223edd33463b9ec704a4e21cd3679415270a
shiva <ms454431@gmail.com>
2024-05-20 19:56:22 +0530

    merged with main with conflicts resolved

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,8 +16,6 @@ import "../../public/assets/css/style.css";
 import "react-toastify/dist/ReactToastify.css";
 import ScrollProgress from "@/components/common/ScrollProgress";
 import useWow from "@/hooks/useWow";
-import Script from "next/script";
-import { GoogleTagManager } from "@next/third-parties/google";
 import { ToastContainer } from "react-toastify";
 
 const inter = Inter({
@@ -55,15 +53,6 @@ export default function RootLayout({ children }) {
         {children}
         <ToastContainer />
       </body>
-      <Script id="gtm" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-W2K58FQ3');
-      `}
-      </Script>
       {/* <script
         type="text/javascript"
         src="https://widget.clutch.co/static/js/widget.js"
