6edcd7a1e16675348aa7e7b84da576aa229ca0a3
shiva <ms454431@gmail.com>
2024-06-03 13:01:06 +0530

    social added in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -121,7 +121,7 @@ const Footer = () => {
                       dream.
                     </p>
                   </div>
-                  <ul className="rating-area">
+                  {/* <ul className="rating-area">
                     <li>
                       <a href="https://clutch.co/" className="single-rating">
                         <div className="review">
@@ -191,6 +191,53 @@ const Footer = () => {
                           </ul>
                           <span>(50 reviews)</span>
                         </div>
+                      </a>
+                    </li>
+                  </ul> */}
+                  <ul className="social-list d-flex gap-3 flex-wrap">
+                    <li>
+                      <a
+                        style={{ color: "#2a82dc !important" }}
+                        href="https://www.linkedin.com/company/metageekstech/"
+                      >
+                        <i className="bi bi-linkedin" />
+                        <span className="ms-1">LinkedIn</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        style={{ color: "#2a82dc !important" }}
+                        href="https://www.facebook.com/metageeks.tech/"
+                      >
+                        <i className="bi bi-facebook" />
+                        <span className="ms-1">Facebook</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        style={{ color: "#2a82dc !important" }}
+                        href="https://x.com/MetaGeeks_tech?t=fwt-7ZmA6NsSFV6J2dcN3w&s=09"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width={18}
+                          height={18}
+                          fill="currentColor"
+                          className="bi bi-twitter-x"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
+                        </svg>
+                        <span className="ms-1">Twitter</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        style={{ color: "#2a82dc !important" }}
+                        href="https://www.instagram.com/metageeks.tech?utm_source=qr&igsh=MWI3YWdjbzdtcWhzZw=="
+                      >
+                        <i className="bi bi-instagram" />
+                        <span className="ms-1">Instagram</span>
                       </a>
                     </li>
                   </ul>
