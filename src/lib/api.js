8f3cc96dfde031ed11259164cdd38d500898b1c4
shiva <ms454431@gmail.com>
2024-06-07 15:39:38 +0530

    contact form testing 3

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,6 +4,7 @@ export const sendContactForm = async (data) =>
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json", Accept: "application/json" },
   }).then((res) => {
-    if (!res.ok) throw new Error("Failed to send message");
+    // if (!res.ok) throw new Error("Failed to send message");
+    console.log(res);
     return res.json();
   });
