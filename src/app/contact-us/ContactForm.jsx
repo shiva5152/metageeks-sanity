bf5c78455349a150ac8b9c81e5402059412caaba
shiva <ms454431@gmail.com>
2024-06-08 19:30:28 +0530

    model close btn redefined

--- src/app/contact-us/ContactForm.jsx
+++ src/app/contact-us/ContactForm.jsx
@@ -16,7 +16,7 @@ const schema = Yup.object().shape({
   message: Yup.string().required().min(50).label("message"),
 });
 
-const ContactForm = ({ isAddStyle, setPopup }) => {
+const ContactForm = ({ isAddStyle, setPopup, showCloseButton }) => {
   const {
     register,
     handleSubmit,
@@ -73,6 +73,7 @@ const ContactForm = ({ isAddStyle, setPopup }) => {
       <div className="contact-form-wrap">
         <div className="contact-form-area">
           <p style={{ fontWeight: "bold" }}>Your Success Starts Here!</p>
+
           <form onSubmit={onSubmitWithCaptchaCheck}>
             <div className="row">
               <div className="col-lg-6 mb-16">
@@ -153,6 +154,19 @@ const ContactForm = ({ isAddStyle, setPopup }) => {
           </form>
         </div>
       </div>
+      {showCloseButton ? (
+        <button id={"closeButton"} onClick={() => setPopup(false)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="#000000"
+            viewBox="0 0 256 256"
+          >
+            <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
+          </svg>
+        </button>
+      ) : null}
     </div>
   );
 };
--- src/components/banner/GetInTouch.jsx
+++ src/components/banner/GetInTouch.jsx
@@ -5,8 +5,12 @@ const GetInTouch = ({ setPopup }) => {
   return (
     <div id="overlay">
       <div id="modal">
-        <ContactForm isAddStyle={true} setPopup={setPopup} />
-        <button id={"closeButton"} onClick={() => setPopup(false)}>
+        <ContactForm
+          isAddStyle={true}
+          showCloseButton={true}
+          setPopup={setPopup}
+        />
+        {/* <button id={"closeButton"} onClick={() => setPopup(false)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="32"
@@ -16,7 +20,7 @@ const GetInTouch = ({ setPopup }) => {
           >
             <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
           </svg>
-        </button>
+        </button> */}
       </div>
     </div>
   );
